41fb909fcf00053db86a9b7d62d59198f70c79d2
Jagamohan-81 <97526535+Jagamohan-81@users.noreply.github.com>
2022-03-03 06:24:02 +0530

    Add files via upload

---
+++ divide.js
@@ -0,0 +1,3 @@
+module.exports.divide=function devide(x,y){
+    return x/y
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+const { add } = require("./add");
+const { divide } = require("./divide");
+const { multiply } = require("./multiply")
+const { subtract } = require("./subtract")
+
+// add=require("add");
+console.log(add(5,6))
+console.log(divide(5,6))
+console.log(multiply(5,6))
+console.log(subtract(5,6))
